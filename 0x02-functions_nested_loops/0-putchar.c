9f6189f37b69a188361db85d6e9fd8aa70e78888
Pharm-Engr-Amy <lawalaminat144@gmail.com>
2022-09-13 21:34:32 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * * main - prints Putchar
- * *
- * *Description: prints _putchar
- * *
- * *Return: 0 (Success)
- * */
+ * main - prints Putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 putchar('');
